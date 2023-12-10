63bffbc0eb116aeaa83e5f5a5f4fddb11f6b0714
kanseidorifto <39811215+kanseidorifto@users.noreply.github.com>
2023-12-10 22:38:23 +0200

    fix error displaying

--- petto-frontend/src/utils/recognizePet.js
+++ petto-frontend/src/utils/recognizePet.js
@@ -3,16 +3,11 @@ import axios from 'axios';
 const recognitionAPIURL = process.env.VITE_APP_RECOGNITION_API_URL; //'/api';
 
 export const recognizePet = async (formData) => {
-	try {
-		const response = await axios.post(`${recognitionAPIURL}/recognize`, formData, {
-			headers: {
-				'Content-Type': 'multipart/form-data',
-			},
-		});
+	const response = await axios.post(`${recognitionAPIURL}/recognize`, formData, {
+		headers: {
+			'Content-Type': 'multipart/form-data',
+		},
+	});
 
-		return response.data;
-	} catch (error) {
-		console.error('Error recognizing pet:', error);
-		return null;
-	}
+	return response.data;
 };
